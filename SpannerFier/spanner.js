2cf06b0fdb02c4e9dab9900e98707be6aca91251
Nathan G <gellienathan@hotmail.com.au>
2018-09-09 19:27:01 +1000

    initial commit, basics of a javascript program that will return a text file with spans

---
+++ SpannerFier/spanner.js
@@ -0,0 +1,24 @@
+/* A web tool to separate given text into a String, where each character is enclosed in <span>(char)</span> */
+
+
+
+function spannerfy {
+    var inputText = String.trim(document.forms['spanner']['spannerinput'].value);
+    var spanText = [];
+    var charArray = inputText.split("");
+
+    for (i=0; i<charArray.length; i++) {
+        spanText.push('<span>');
+        spanText.push(charArray[i]);
+        spanText.push('</span>');
+    }
+
+    var returnText = "";
+
+    for (i=0; i<spanText.length; i++) {
+        returnText += spanText[i];
+    }
+
+    alert(returnText);
+}
+
